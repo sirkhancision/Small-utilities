b46ac65fe6bd2a6e0261d173791f2d9eb30ff8e5
Jorge <jsantiago12tone@gmail.com>
2021-06-01 01:39:34 -0300

    Updated README.md and added linux-ver.sh

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ## Small utilities
 
 These are simply some small programs that I wrote to perform basic actions on my system.
+
+>``fracsimp.c``: C program to simplify fractions.
+
+>``mdc.c``: C program to calculate the greatest common divisor from two integers.
+
+>``ram_speed.c``: C program to calculate a RAM's Clock Speed.
+
+>``randomcase.c``: C program to randomize characters' case in a text file, or through stdin.
+
+>``linux-ver.sh``: ZSH (or bash, but you'd need to include ``#!/bin/bash`` at the first line) script to notify you when a new Linux release gets uploaded to Github.
---
+++ linux-ver.sh
@@ -0,0 +1,26 @@
+# you need run this script only once for it to 
+
+REPO=https://github.com/torvalds/linux
+# gets full tags list from REPO, in ascending order, then isolates the last line (the latest tag)
+# and isolates the string starting at the 12th character from the second word, which so happens
+# to be where the actual tag name starts
+tag=$(git ls-remote $REPO | sort -Vk2 | tail -1 | awk '{print $2}' | awk '{ print substr ($0, 12 ) }')
+garbage='^{}'
+tmpfile="/tmp/linux-latest"
+
+# checks if the tag name has that weird substring, and if so, removes it
+if [[ "$tag" == *"$garbage"* ]]; then
+    tag=$(echo $tag | tr -d $garbage)
+fi
+
+# in case the file holding the latest version name doesn't exist, it's created
+# if the latest release also doesn't match the current tag name, it overwrites its value
+# at last, if none of these apply, the script is ended
+if [[ $(! test -f "$tmpfile") ]] || [[ "$(cat $tmpfile)" != "$tag" ]]; then
+    echo "$tag" > "$tmpfile"
+else
+    exit 1
+fi
+
+# sends a notification with the current Linux version tag, lasting for 20s and using the tux icon
+notify-send -a sirkhancision -i tux "Linux version update:" -t 20000 "Latest version: $tag"
