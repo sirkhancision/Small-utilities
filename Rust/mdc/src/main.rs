722385b175a4a518347a90384961e09b0eff8c26
Jorge Santiago <jsantiago12tone@gmail.com>
2021-11-01 03:15:58 -0300

    Changed from stdin input to input from command arguments. Also made a struct for mdc, so as to have the methods be closely linked to each other.

--- Rust/mdc/src/main.rs
+++ Rust/mdc/src/main.rs
@@ -1,61 +1,62 @@
-use std::{io, mem::swap, process};
+use std::{env, mem::swap, process};
 
 fn main() {
-    let mut input = String::new();
+    let args: Vec<String> = env::args().collect();
 
-    println!("Digite dois números para calcular o MDC:");
+    if args.len() == 3 {
+        let input = Mdc::new(&args).unwrap_or_else(|err| {
+            eprintln!("Execução abortada: {}", err);
+            process::exit(1);
+        });
 
-    if let Err(e) = io::stdin().read_line(&mut input) {
-        eprintln!(
-            "Execução abortada: erro ao ler texto da entrada padrão\n{}",
-            e
-        );
-        process::exit(1);
-    }
-    let input = input.trim();
-
-    if !input.is_empty() {
         println!(
             "MDC: {}",
-            mdc(slice_input(input).unwrap_or_else(|err| {
-                eprintln!("Execução abortada: {}", err);
-                process::exit(1);
-            }))
-            .unwrap_or_else(|err| {
+            Mdc::calculate(input).unwrap_or_else(|err| {
                 eprintln!("Execução abortada: {}", err);
                 process::exit(1);
             })
         );
+    } else {
+        eprintln!("Argumentos insuficientes passados ao programa");
+        process::exit(1);
     }
 }
 
-fn mdc(mut input: (u128, u128)) -> Result<u128, &'static str> {
-    if input.0 == 0 || input.1 == 0 {
-        return Err("divisão por zero não é permitida");
-    } else if input.1 > input.0 {
-        swap(&mut input.0, &mut input.1);
-    }
-
-    // Euclidean algorithm
-    while input.0 % input.1 != 0 {
-        swap(&mut input.0, &mut input.1);
-        input.1 %= input.0;
-    }
-
-    Ok(input.1)
+struct Mdc {
+    first_var: u128,
+    second_var: u128,
 }
 
-// Slices the input string and outputs a u128 tuple, with
-// the two numbers for calculating the GDC
-fn slice_input(input_string: &str) -> Result<(u128, u128), &str> {
-    for (i, char) in input_string.chars().enumerate() {
-        if char.is_whitespace() {
-            let x = input_string[..i].parse().expect("x não é inteiro");
-            let y = input_string[i + 1..].parse().expect("y não é inteiro");
+impl Mdc {
+    fn new(input: &Vec<String>) -> Result<Self, &str> {
+        let x: u128 = match input[1].as_str().parse() {
+            Err(_e) => return Err("x não é inteiro"),
+            Ok(n) => n,
+        };
+        let y: u128 = match input[2].as_str().parse() {
+            Err(_e) => return Err("y não é inteiro"),
+            Ok(n) => n,
+        };
 
-            return Ok((x, y));
+        Ok(Mdc {
+            first_var: x,
+            second_var: y,
+        })
+    }
+
+    fn calculate(mut input: Self) -> Result<u128, &'static str> {
+        if input.first_var == 0 || input.second_var == 0 {
+            return Err("divisão por zero não é permitida");
+        } else if input.second_var > input.first_var {
+            swap(&mut input.first_var, &mut input.second_var);
         }
-    }
 
-    Err("não há espaço separando o primeiro número do segundo")
+        // Euclidean algorithm
+        while input.first_var % input.second_var != 0 {
+            swap(&mut input.first_var, &mut input.second_var);
+            input.second_var %= input.first_var;
+        }
+
+        Ok(input.second_var)
+    }
 }
