f3ca2221b687ff442175277173504e9f91deec06
Jorge Santiago <jsantiago12tone@gmail.com>
2021-11-01 20:03:14 -0300

    (fullwidth_converter) Changed from stdin input to input from CLI arguments and added extra error handling.

--- Rust/fullwidth_converter/src/main.rs
+++ Rust/fullwidth_converter/src/main.rs
@@ -1,22 +1,16 @@
-use std::io;
+use std::{env, process};
 use unicode_hfwidth::to_fullwidth;
 use unidecode::unidecode;
 
 fn main() {
-    println!("Insira o texto a passar para caracteres fullwidth:");
+    let args: Vec<String> = env::args().collect();
 
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("erro ao ler texto da entrada padrão");
+    if args.len() > 1 {
+        // convert the string to ascii-only,
+        // so as to be perfectly convertable to fullwidth chars
+        let text = unidecode(&args[1]);
 
-    // convert the trimmed string to ascii-only,
-    // so as to be perfectly convertable to fullwidth chars
-    input = unidecode(&input);
-    let input = input.trim();
-
-    if !input.is_empty() {
-        for char in input.chars() {
+        for char in text.chars() {
             if char.is_ascii_alphabetic() || char.is_ascii_digit() {
                 if let Some(c) = to_fullwidth(char) {
                     print!("{}", c);
@@ -25,6 +19,9 @@ fn main() {
                 print!("{}", char);
             }
         }
+        print!("\n");
+    } else {
+        eprintln!("Argumentos insuficientes passados ao programa");
+        process::exit(1);
     }
-    print!("\n");
 }
