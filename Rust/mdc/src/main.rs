845b51d6d110b19fbab4f0ab7cf865f93dd0d0ed
Jorge Santiago <jsantiago12tone@gmail.com>
2021-11-01 19:17:21 -0300

    Added help flag for CLI arguments.

--- Rust/mdc/src/main.rs
+++ Rust/mdc/src/main.rs
@@ -3,7 +3,13 @@ use std::{env, mem::swap, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 3 {
+    if args.len() == 2 && args.iter().any(|h| h == "-h" || h == "--help") {
+        println!(
+            "Calculadora de MDC (Máximo Divisor Comum)\n\
+ Formato: x y (Exemplo: 10 5)\n
+Argumentos possíveis: \"-h\"/\"--help\" - Exibe esse texto de ajuda."
+        );
+    } else if args.len() == 3 {
         let input = Mdc::new(&args).unwrap_or_else(|err| {
             eprintln!("Execução abortada: {}", err);
             process::exit(1);
