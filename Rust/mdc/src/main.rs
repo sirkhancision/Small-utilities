2149757b2ded025c3896f5135d590647c909acea
Jorge Santiago <jsantiago12tone@gmail.com>
2021-11-01 02:18:53 -0300

    Better error handling, such as actually printing to stderr and having a more idiomatic way of conveying these errors, in the code.

--- Rust/mdc/src/main.rs
+++ Rust/mdc/src/main.rs
@@ -1,25 +1,39 @@
-use std::{io, mem::swap};
+use std::{io, mem::swap, process};
 
 fn main() {
     let mut input = String::new();
 
     println!("Digite dois números para calcular o MDC:");
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("erro ao ler texto da entrada padrão");
+    if let Err(e) = io::stdin().read_line(&mut input) {
+        eprintln!(
+            "Execução abortada: erro ao ler texto da entrada padrão\n{}",
+            e
+        );
+        process::exit(1);
+    }
     let input = input.trim();
 
     if !input.is_empty() {
-        println!("MDC: {}", mdc(slice_input(input)));
+        println!(
+            "MDC: {}",
+            mdc(slice_input(input).unwrap_or_else(|err| {
+                eprintln!("Execução abortada: {}", err);
+                process::exit(1);
+            }))
+            .unwrap_or_else(|err| {
+                eprintln!("Execução abortada: {}", err);
+                process::exit(1);
+            })
+        );
     }
 }
 
-fn mdc(mut input: (u128, u128)) -> u128 {
-    if input.1 > input.0 {
+fn mdc(mut input: (u128, u128)) -> Result<u128, &'static str> {
+    if input.0 == 0 || input.1 == 0 {
+        return Err("divisão por zero não é permitida");
+    } else if input.1 > input.0 {
         swap(&mut input.0, &mut input.1);
-    } else if input.1 == 0 && input.0 == 0 {
-        panic!("entrada inválida");
     }
 
     // Euclidean algorithm
@@ -28,20 +42,20 @@ fn mdc(mut input: (u128, u128)) -> u128 {
         input.1 %= input.0;
     }
 
-    input.1
+    Ok(input.1)
 }
 
 // Slices the input string and outputs a u128 tuple, with
 // the two numbers for calculating the GDC
-fn slice_input(input_string: &str) -> (u128, u128) {
+fn slice_input(input_string: &str) -> Result<(u128, u128), &str> {
     for (i, char) in input_string.chars().enumerate() {
         if char.is_whitespace() {
             let x = input_string[..i].parse().expect("x não é inteiro");
             let y = input_string[i + 1..].parse().expect("y não é inteiro");
 
-            return (x, y);
+            return Ok((x, y));
         }
     }
 
-    panic!("não há espaço separando primeiro número do segundo");
+    Err("não há espaço separando o primeiro número do segundo")
 }
