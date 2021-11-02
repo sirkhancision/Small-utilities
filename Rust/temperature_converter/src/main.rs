11993226d91e5f5ccb18223eade55b0759808036
Jorge Santiago <jsantiago12tone@gmail.com>
2021-11-01 21:40:13 -0300

    (Rust: temperature converter) Made the program's code more idiomatic.

--- Rust/temperature_converter/src/main.rs
+++ Rust/temperature_converter/src/main.rs
@@ -2,7 +2,6 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-
     println!("Conversão de F° para C° [1] ou de C° para F° [2]?");
 
     io::stdin()
@@ -10,26 +9,24 @@ fn main() {
         .expect("Erro ao ler input");
     let input: f64 = input.trim().parse().expect("input não é um float válido");
 
-    if input >= 1.0 && input <= 2.0 {
+    if input == 1.0 || input == 2.0 {
         println!("Digite a temperatura:");
-        conversion(input);
+        println!("{}", conversion(input));
     } else {
         println!("Opção inválida");
     }
 }
 
-fn conversion(option: f64) {
-    // the temperature input
+fn conversion(option: f64) -> String {
     let mut input = String::new();
-    // the converted temperature output
-    let output;
-    // the temperature unit suffix
-    let suffix;
 
     io::stdin()
         .read_line(&mut input)
         .expect("Erro ao ler input");
     let input: f64 = input.trim().parse().expect("input não é um float válido");
+
+    let output;
+    let suffix;
 
     if option == 1.0 {
         output = 5.0 / 9.0 * (input - 32.0);
@@ -39,5 +36,5 @@ fn conversion(option: f64) {
         suffix = "F°";
     }
 
-    println!("{:.1} {}", output, suffix);
+    format!("{:.1} {}", output, suffix)
 }
