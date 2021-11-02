06a1e204d688fe79c000acd815129f0d9cbba726
Jorge Santiago <jsantiago12tone@gmail.com>
2021-11-01 21:39:11 -0300

    (Rust: ram speed) Changed the order of stuff to have better ownership within the program.

--- Rust/ram_speed/src/main.rs
+++ Rust/ram_speed/src/main.rs
@@ -2,27 +2,28 @@ use std::io;
 
 fn main() {
     let mut cl = String::new();
-    let mut mhz = String::new();
-
     println!("Digite o valor do Ciclo-CL:");
 
     io::stdin()
         .read_line(&mut cl)
         .expect("Erro ao ler Ciclo-CL");
+    
     let cl: f64 = cl
         .trim()
         .parse()
-        .expect("Ciclo-CL dado não é um número inteiro");
-
+        .expect("ciclo cl dado não é um número inteiro");
+    
+    let mut mhz = String::new();
     println!("Agora, digite o valor do MHz-RAM:");
 
     io::stdin()
         .read_line(&mut mhz)
         .expect("Erro ao ler MHz-RAM");
+    
     let mhz: f64 = mhz
         .trim()
         .parse()
-        .expect("MHz-RAM dado não é um número inteiro");
+        .expect("mhz da ram dado não é um número inteiro");
 
     let clock_speed = (2.0 * cl * 1E9) / (mhz * 1E6);
 
