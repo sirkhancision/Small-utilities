3f5cd57da21cc30961123ff0ef2c3739cf28475f
Jorge Santiago <jsantiago12tone@gmail.com>
2021-11-01 21:35:02 -0300

    (christmas carol) Made the iteration more compact and idiomatic, along with changing from an empty println to printing a newline character.

--- Rust/christmas_carol/src/main.rs
+++ Rust/christmas_carol/src/main.rs
@@ -17,14 +17,11 @@ fn main() {
         "12 drummers drumming",
     ];
 
-    let i = 0;
-    let j = 0;
-
-    for i in i..days.len() {
+    for i in 0..days.len() {
         println!("On the {} day of Christmas", days[i]);
         println!("my true love sent to me:");
 
-        for j in (j..=i).rev() {
+        for j in (0..=i).rev() {
             if j == 0 && i > 0 {
                 print!("and a ");
             } else if i == 0 {
@@ -35,7 +32,7 @@ fn main() {
         }
 
         if i + 1 < days.len() {
-            println!("");
+            print!("\n");
         }
     }
 }
